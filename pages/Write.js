08d6de1bcefc53331f735dd7f1e804677a45ca58
vezzita <minyoungkim.vezzita@gmail.com>
2017-11-16 00:23:12 +0900

    ajax sk planet weather api 획득

--- pages/Write.js
+++ pages/Write.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { StyleSheet, Image, View, Text, TextInput } from 'react-native';
+import axios from 'axios'
 import style from './Style'
+
+const WEATHER_APP_KEY = '8e910f9c-3965-3d3c-91aa-dda9db46227f';
 
 const styles = StyleSheet.create(style.commonStyle);
 
 export default class Write extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { 
+      text: '',
+      skyData: [],
+      geoData: {
+        lat: '',
+        lot: '',
+
+      },
+    };
   }
 
   static navigationOptions = {
@@ -20,6 +31,27 @@ export default class Write extends React.Component {
     ),
   };
 
+  componentDidMount() {
+    axios.get(`http://apis.skplanetx.com/weather/current/minutely?version=1&lat=37&lon=127`, {
+      headers: {
+        appKey: WEATHER_APP_KEY,
+      }
+    })
+    .then(res => {
+      this.setState({
+        skyData: res.data.weather.minutely[0].sky,
+        geoData: {
+          lat: res.data.weather.minutely[0].station.latitude,
+          lot: res.data.weather.minutely[0].station.longitude,
+        }
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
+  
+
   render() {
     return (
       <View style={styles.container}>
@@ -30,6 +62,8 @@ export default class Write extends React.Component {
           editable = {true}
           value={this.state.text}
         />
+        <Text>{this.state.skyData.name}</Text>
+        <Text>{this.state.geoData.lat}{this.state.geoData.lot}</Text>
       </View>
     );
   }
