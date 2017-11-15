58e45279a356b16e728b3296c8772e8885848379
vezzita <minyoungkim.vezzita@gmail.com>
2017-11-15 22:12:54 +0900

    Style.js 수정, common style, diary page style 통합

--- pages/Style.js
+++ pages/Style.js
@@ -1,18 +1,47 @@
-const styles = {
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+const style = {
+  commonStyle: {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      color: '#FF5733',
+      fontSize: 16,
+      fontWeight: '300',
+      marginBottom: 20,
+    },
+    textInput: {
+      width: '90%',
+      height: '70%',
+      borderRadius: 10,
+      backgroundColor: '#f8f8f8',
+    },
+    icon: {
+      width: 20,
+      height: 20,
+    },
   },
-  title: {
-    color: '#FF5733',
-    fontSize: 16,
-    fontWeight: '300',
-  },
-  icon: {
-    width: 20,
-    height: 20,
-  },
+  diaryStyle: {
+    list: {
+      width: '90%',
+      paddingTop: 15,
+      paddingBottom: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: '#efefef',
+    },
+    listTitle: {
+      fontSize: 16,
+      fontWeight: '300',
+      color: '#888'
+    },
+    listDate: {
+      marginTop: 10,
+      fontSize: 12,
+      fontWeight: '300',
+      color: '#ccc',
+    }
+  }
 };
 
-export default styles;
+export default style;
