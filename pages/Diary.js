496ed7dad873ebbd04076c56cc51ed2366541c63
vezzita <minyoungkim.vezzita@gmail.com>
2017-11-15 22:09:43 +0900

    Diary화면 일기 목록 Dummy data추가->레이아웃 작업중

--- pages/Diary.js
+++ pages/Diary.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { StyleSheet, Image, View, Text } from 'react-native';
 import style from './Style'
 
-const styles = StyleSheet.create(style);
+const styles = StyleSheet.create(style.commonStyle);
+const diaryStyles = StyleSheet.create(style.diaryStyle);
+const dummyData = [
+  {
+    id: 1,
+    title: '日本旅行1日目 東京',
+    date: '2017/11/01'
+  },
+  {
+    id: 2,
+    title: '日本旅行3日目 東京',
+    date: '2017/11/03'
+  }
+]
 
 export default class Diary extends React.Component {
   static navigationOptions = {
@@ -18,7 +31,17 @@ export default class Diary extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>오늘은 어떤 추억을 담으시겠어요?</Text>
+        <Text style={styles.title}>추억더미</Text>
+        {dummyData.map((item, index) => (
+          <View 
+            style={diaryStyles.list}
+            key={index}
+            >
+            <Text style={diaryStyles.listTitle}>{item.title}</Text>
+            <Text style={diaryStyles.listDate}>{item.date}</Text>
+          </View>
+
+        ))}
       </View>
     );
   }
